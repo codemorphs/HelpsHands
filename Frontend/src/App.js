d4bf77f5e731ecb653568d811ec59f725e4be4c2
IT21806568 <110652033+IT21806568@users.noreply.github.com>
2023-10-28 18:44:45 +0530

    Navbar installed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,19 @@
 // import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar";
-
+import Home from "./Components/Home";
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+       <BrowserRouter>
+          <Navbar />
+          <div className='pages'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+            
+            </Routes>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
