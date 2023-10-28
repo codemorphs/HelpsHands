54707378f8de46f801601ae1bfad5c1316f082ae
IT21806568 <110652033+IT21806568@users.noreply.github.com>
2023-10-28 14:00:38 +0530

    Home page frontend

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -1,41 +1,86 @@
-import React from 'react'
-import './Home.css'
+import React, { useEffect, useState } from 'react';
+
+import './Home.css';
 
 const HomePage = () => {
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    // Use a setTimeout to delay the appearance of the banner for the fade-in effect
+    setTimeout(() => {
+      setShowBanner(true);
+    }, 100);
+  }, []);
   return (
     <div className="home-page">
-      <header>
-        <h1>Welcome to Our Psychological Clinic</h1>
+      
+      <header className="banner">
+        <img src="/banner.jpg" alt="Banner" />
+        <div className="banner-content">
+          <h1>WELCOME TO THE <br />HELPING HAND</h1><br />
+          <h2>PSYCHOLOGICAL HOSPITAL</h2>
+          <a href="#contact" className="contact-button">
+            Contact Us
+          </a>
+        </div>
       </header>
 
       <main>
-        <section className="intro-section">
-          <div className="container">
-            <h2>About Us</h2>
+      <section class="we-offer-area text-center bg-gray">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="site-heading text-center">
+            <h2>Choose Our <span>Medical Services</span></h2>
+            <h4>provide a wide range of mental health services to people of all ages and backgrounds</h4>
+          </div>
+        </div>
+      </div>
+      <div class="row our-offer-items less-carousel">
+        <div class="col-md-4 col-sm-6 equal-height">
+          <div class="item">
+            <div class="service-icon">
+              <img src="/Therapy.jpg" alt="Therapy" />
+            </div>
+            <br />
+            <h4>Therapy</h4>
             <p>
-              We are a dedicated team of professionals who provide caring and
-              effective psychological services. Our clinic is committed to
-              helping individuals, couples, and families navigate life's
-              challenges.
+              Psychologists can provide a variety of therapies to help people with mental health problems. Some common types of therapy include cognitive-behavioral therapy (CBT), interpersonal therapy (IPT), and psychodynamic therapy.
             </p>
           </div>
-        </section>
+        </div>
 
-        <section className="services-section">
-          <div className="container">
-            <h2>Our Services</h2>
-            <ul>
-              <li>Counseling and Therapy</li>
-              <li>Individual and Group Sessions</li>
-              <li>Stress Management</li>
-              <li>Child and Adolescent Counseling</li>
-              <li>Marriage and Family Counseling</li>
-            </ul>
+        <div class="col-md-4 col-sm-6 equal-height">
+          <div class="item">
+            <div class="service-icon">
+              <img src="/counceling.jpg" alt="Counseling" />
+            </div>
+            <br />
+            <h4>Counseling</h4>
+            <p>
+              Psychologists can also provide counseling to help people with a variety of personal and interpersonal challenges, such as grief and loss, relationship problems, and stress management.
+            </p>
           </div>
-        </section>
+        </div>
+
+        <div class="col-md-4 col-sm-6 equal-height">
+          <div class="item">
+            <div class="service-icon">
+              <img src="/Medication.jpg" alt="Medication Management" />
+            </div>
+            <br />
+            <h4>Medication Management</h4>
+            <p>
+              Psychologists can work with psychiatrists to manage medication for mental health conditions. This may involve prescribing new medications, adjusting dosages, or monitoring side effects.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
       </main>
 
-      <footer>
+      <footer id="contact">
         <div className="container">
           <p>Contact us for an appointment or further information.</p>
           <p>Email: clinic@example.com</p>
