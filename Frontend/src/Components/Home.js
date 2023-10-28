c16d3befee0c0dfd69399d16f6ff7e703f95fde4
Oneth <onethkahandawa@gmail.com>
2023-10-28 19:56:44 +0530

    adding about us section

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import './Home.css';
+import "./Home.css";
 
 const HomePage = () => {
   const [showBanner, setShowBanner] = useState(false);
@@ -13,11 +13,14 @@ const HomePage = () => {
   }, []);
   return (
     <div className="home-page">
-      
       <header className="banner">
         <img src="/banner.jpg" alt="Banner" />
         <div className="banner-content">
-          <h1>WELCOME TO THE <br />HELPING HAND</h1><br />
+          <h1>
+            WELCOME TO THE <br />
+            HELPING HAND
+          </h1>
+          <br />
           <h2>PSYCHOLOGICAL HOSPITAL</h2>
           <a href="#contact" className="contact-button">
             Contact Us
@@ -26,58 +29,106 @@ const HomePage = () => {
       </header>
 
       <main>
-      <section class="we-offer-area text-center bg-gray">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="site-heading text-center">
-            <h2>Choose Our <span>Medical Services</span></h2>
-            <h4>provide a wide range of mental health services to people of all ages and backgrounds</h4>
-          </div>
-        </div>
-      </div>
-      <div class="row our-offer-items less-carousel">
-        <div class="col-md-4 col-sm-6 equal-height">
-          <div class="item">
-            <div class="service-icon">
-              <img src="/Therapy.jpg" alt="Therapy" />
+        <section class="we-offer-area text-center bg-gray">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="site-heading text-center">
+                  <h2>
+                    Choose Our <span>Medical Services</span>
+                  </h2>
+                  <h4>
+                    provide a wide range of mental health services to people of
+                    all ages and backgrounds
+                  </h4>
+                </div>
+              </div>
             </div>
-            <br />
-            <h4>Therapy</h4>
-            <p>
-              Psychologists can provide a variety of therapies to help people with mental health problems. Some common types of therapy include cognitive-behavioral therapy (CBT), interpersonal therapy (IPT), and psychodynamic therapy.
-            </p>
-          </div>
-        </div>
+            <div class="row our-offer-items less-carousel">
+              <div class="col-md-4 col-sm-6 equal-height">
+                <div class="item">
+                  <div class="service-icon">
+                    <img src="/Therapy.jpg" alt="Therapy" />
+                  </div>
+                  <br />
+                  <h4>Therapy</h4>
+                  <p>
+                    Psychologists can provide a variety of therapies to help
+                    people with mental health problems. Some common types of
+                    therapy include cognitive-behavioral therapy (CBT),
+                    interpersonal therapy (IPT), and psychodynamic therapy.
+                  </p>
+                </div>
+              </div>
 
-        <div class="col-md-4 col-sm-6 equal-height">
-          <div class="item">
-            <div class="service-icon">
-              <img src="/counceling.jpg" alt="Counseling" />
-            </div>
-            <br />
-            <h4>Counseling</h4>
-            <p>
-              Psychologists can also provide counseling to help people with a variety of personal and interpersonal challenges, such as grief and loss, relationship problems, and stress management.
-            </p>
-          </div>
-        </div>
+              <div class="col-md-4 col-sm-6 equal-height">
+                <div class="item">
+                  <div class="service-icon">
+                    <img src="/counceling.jpg" alt="Counseling" />
+                  </div>
+                  <br />
+                  <h4>Counseling</h4>
+                  <p>
+                    Psychologists can also provide counseling to help people
+                    with a variety of personal and interpersonal challenges,
+                    such as grief and loss, relationship problems, and stress
+                    management.
+                  </p>
+                </div>
+              </div>
 
-        <div class="col-md-4 col-sm-6 equal-height">
-          <div class="item">
-            <div class="service-icon">
-              <img src="/Medication.jpg" alt="Medication Management" />
+              <div class="col-md-4 col-sm-6 equal-height">
+                <div class="item">
+                  <div class="service-icon">
+                    <img src="/Medication.jpg" alt="Medication Management" />
+                  </div>
+                  <br />
+                  <h4>Medication Management</h4>
+                  <p>
+                    Psychologists can work with psychiatrists to manage
+                    medication for mental health conditions. This may involve
+                    prescribing new medications, adjusting dosages, or
+                    monitoring side effects.
+                  </p>
+                </div>
+              </div>
             </div>
-            <br />
-            <h4>Medication Management</h4>
-            <p>
-              Psychologists can work with psychiatrists to manage medication for mental health conditions. This may involve prescribing new medications, adjusting dosages, or monitoring side effects.
-            </p>
+          </div>
+        </section>
+
+        <div class="we-are-block">
+          <div id="about-us-section">
+            <div class="about-us-image">
+              <img
+                src="https://digitalupgrade.com/images/lobbyimage_1.jpg"
+                width="808"
+                height="458"
+                alt="Lobby Image"
+              />
+            </div>
+
+            <div class="about-us-info">
+              <h2>Your mental health matters.</h2>
+
+              <p>
+                Psychology is the scientific study of the mind and behavior. It
+                explores the complex processes that shape our thoughts,
+                emotions, and actions. Psychologists seek to understand how
+                individuals think, feel, and interact with the world, as well as
+                how these processes influence our well-being. It encompasses a
+                wide range of topics, from cognitive processes and emotional
+                well-being to clinical disorders and social interactions.
+                Psychology plays a crucial role in helping us better understand
+                ourselves and others, ultimately contributing to personal
+                growth, mental health, and improved quality of life.
+              </p>
+
+              {/* <a href="#" title="About Us Button">
+                ABOUT US
+              </a> */}
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </section>
       </main>
 
       <footer id="contact">
