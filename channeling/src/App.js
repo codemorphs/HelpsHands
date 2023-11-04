bf3a3ddcd11bad08b5d74bf8122a3be8d99f3b25
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-11-04 21:36:32 +0530

    create react for channeling

---
+++ channeling/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Channeling from './Component/Channeling/channeling';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/channeling" element={< Channeling/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ channeling/src/Component/Channeling/channeling.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./channeling.css";
+
+function Channeling() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Channeling;
