0ec09584de433178d042540d3ff2bea92867cd39
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-12-01 23:28:52 +0530

    added login backend

--- Frontend/src/Components/LoginPage/LoginPage.js
+++ Frontend/src/Components/LoginPage/LoginPage.js
@@ -1,29 +1,46 @@
-import React from "react";
+
+import React, { useState } from 'react';
+import axios from 'axios';
 import "./LoginPage.css";
 
 function LoginPage(){
+
+    const [Email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+        console.log("email", Email);
+        const response = await axios.post('http://localhost:3010/login', {
+            Email,
+            password,
+        });
+
+        if (response.data.success) {
+            alert('Login successful');
+        } else {
+            alert('Login failed. Please check your credentials.');
+        }
+        } catch (error) {
+        console.error('Error during login:', error);
+        }
+    };
+
+
     return (
         <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                        <h3>Sign In</h3>
-                        <form id="login-form" action="#" method="#">
-                            <div className="emailField">
-                                <input type="email" id="email" name="email" required placeholder="Email"/>
-                            </div>
-                            <div className="passwordField">
-                                <input type="password" id="password" name="password" required placeholder="Password"/>
-                            </div>
-                            <div className="SignBtn">
-                                <button type="submit">Sign In</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+          <h1>Login</h1>
+          <div>
+            <label>Email:</label>
+            <input type="text" value={Email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button onClick={handleLogin}>Login</button>
         </div>
-    );
+      );
 }
 
 export default LoginPage;
